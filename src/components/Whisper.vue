c6d443ccedaf8a4dd422a9c775ee1ed1bf9d699d
renjiangwei <1223995087@qq.com>
2023-12-13 09:11:14 +0800

    fix:移动端bug

--- src/components/Whisper.vue
+++ src/components/Whisper.vue
@@ -3,7 +3,8 @@
 import { transcribeAudio } from '@/services/whisperService';
 import type { OutputType } from '@/services/whisperService';
 import { reactive, ref, onMounted, nextTick } from 'vue';
-import { AudioOutlined } from '@ant-design/icons-vue'
+import { AudioOutlined, UploadOutlined } from '@ant-design/icons-vue'
+import { isMobile } from '@/utils/agent'
 
 enum MessageStatus {
   'recording', 'doing', 'done'
@@ -64,12 +65,14 @@ const startY = ref(0);
 let stopType: 'stop' | 'cancel' | '' = ''
 const threshold = 100; // 上滑取消的阈值，可根据需要调整
 
+const recordStartTime = ref(0)
 const startRecording = async (event) => {
+  recordStartTime.value = new Date().getTime()
   if (!navigator.mediaDevices) {
     console.error('MediaDevices API not supported.');
+    alert('MediaDevices API not supported.')
     return;
   }
-
   isRecording.value = true;
   startY.value = event.type === 'mousedown' ? event.clientY : event.touches[0].clientY;
 
@@ -107,11 +110,20 @@ const startRecording = async (event) => {
 
       // 清空 chunks 以便下次录音
       audioChunks.value = [];
+      
+      // 暂时下载音频文件
+      // const u = URL.createObjectURL(audioBlob)
+      // const a = document.createElement('a')
+      // a.download = 'a.wav';
+      // a.href = u;
+      // a.click()
+      // URL.revokeObjectURL(u)
 
       requestAndtranscribeAudio(audioBlob).then(text => {
         item.audio = audio
         item.content = text
         item.status = MessageStatus.done
+        item.url = audioUrl;
       })
     };
 
@@ -121,8 +133,21 @@ const startRecording = async (event) => {
   }
 };
 
-const stopRecording = () => {
+const stopRecording = (event: MouseEvent | TouchEvent) => {
+  console.log(event, 'dd')
+  showTip.value = false;
   if (!isRecording.value) return;
+  if (new Date().getTime() - recordStartTime.value < 1000) {
+    // 1s内松手就取消录制
+    handleRecordingCancel()
+    return
+  }
+  const yOffset = event.type === 'mouseup' ? event.clientY : event.changedTouches[0].clientY;
+  if (Math.abs(yOffset - startY.value) > threshold) {
+    // 上滑距离超过阈值，取消录音
+    handleRecordingCancel();
+    return
+  }
   stopType = 'stop'
   if (mediaRecorder.value && mediaRecorder.value.state === 'recording') {
     mediaRecorder.value.stop();
@@ -130,15 +155,36 @@ const stopRecording = () => {
   isRecording.value = false;
 };
 
+const showTip = ref(false)
 const handleTouchMove = (event) => {
   if (!isRecording.value) return;
 
   const touchY = event.touches[0].clientY;
   if (startY.value - touchY > threshold) {
     // 上滑距离超过阈值，取消录音
-    handleRecordingCancel();
+    showTip.value = true
+    // handleRecordingCancel();
+  } else {
+    showTip.value = false;
   }
 };
+
+onMounted(() => {
+  const dom = document.querySelector('#recordBtnRef') as HTMLDivElement
+  if (isMobile()) {
+    dom?.addEventListener('touchstart', startRecording)
+    dom?.addEventListener('touchend', stopRecording)
+    dom?.addEventListener('touchmove', (e) => {
+      e.preventDefault()
+      handleTouchMove(e)
+    })
+    
+  } else {
+    dom?.addEventListener('mousedown', startRecording)
+    dom?.addEventListener('mouseup', stopRecording)
+
+  }
+})
 
 const handleRecordingCancel = () => {
   stopType = 'cancel'
@@ -153,10 +199,45 @@ const handleRecordingCancel = () => {
 
 const beforeUpload = (file) => {
   // 处理文件上传逻辑
+  // file to blob
+  const type = file.type;
+  const reader = new FileReader();
+  reader.readAsArrayBuffer(file);
+  reader.onload = (e) => {
+    console.log(e, 'dddee')
+    const blob = new Blob([e.target.result], {type: 'audio/wav'});
+    const audioUrl = URL.createObjectURL(blob);
+    const audio = new Audio(audioUrl);
+    messageHistory.value.push({
+      id: messageHistory.value.length + 1,
+      audio: audio,
+      status: MessageStatus.doing,
+      content: '',
+      format: asrSetting.outputFormat
+    })
+    const item = messageHistory.value[messageHistory.value.length - 1];
+    requestAndtranscribeAudio1(blob).then(text => {
+      item.content = text;
+      item.status = MessageStatus.done;
+    })
+  }
   return false; // 阻止组件默认上传行为
 };
 
-const requestAndtranscribeAudio = async (blob) => {
+const requestAndtranscribeAudio1 = async(file) => {
+  try {
+    let formData = new FormData();
+    formData.append('audio_file', file, 'temp.wav');
+    // Call the Whisper API for audio transcription
+    const result = await transcribeAudio(asrSetting, formData);
+    console.log('Transcription result:', result);
+    return result
+    // TODO: Handle the result as needed
+  } catch (error) {
+    console.error('Error transcribing audio:', error);
+  }
+}
+const requestAndtranscribeAudio = async (blob: Blob) => {
   try {
     let formData = new FormData();
     let file = new File([blob], 'temp.wav')
@@ -186,12 +267,15 @@ const innerHeight = ref(window.innerHeight + 'px')
 </script>
 
 <template>
-  <div  :style="{height:innerHeight}" >
+  <div :style="{height:innerHeight}" >
+    <!-- <div v-if="showTip" class="absolute bottom-0 left-0 z-50">松开取消输入</div> -->
     <div class="flex flex-col h-full z-10">
       <!-- 记录区域 -->
       <div class="flex-1 overflow-y-auto p-4 space-y-2 bg-gray-300 items-end" ref="historyAreaRef">
         <div v-for="(message,index) in messageHistory" :key="message.id" class="flex justify-end ">
           <a-card class="max-w-md w-full rounded-lg bg-blue-200" :class="[index === messageHistory.length-1?'z-50':'']">
+            {{ message }}
+            <audio controls :src="message.url"></audio>
             <p v-if="message.status === MessageStatus.recording">录制中...</p>
             <p v-else-if="message.status === MessageStatus.doing">识别中...</p>
             <p v-else>{{ message.content }}</p>
@@ -200,29 +284,32 @@ const innerHeight = ref(window.innerHeight + 'px')
       </div>
       <!-- Recording Mask -->
       <!-- 发送/上传区域 -->
-      <div class="shrink-0 p-4 border-t border-gray-200">
+      <div class="shrink-0 p-4 border-t border-gray-200 select-none">
         <div v-if="isRecordingMode" class="flex justify-center items-center h-24 relative z-50">
           <!-- 录制按钮 -->
-          <a-button type="primary"  shape="circle" style="height: 72px;width:72px"
-            @mousedown="startRecording" 
+          <a-button type="primary" shape="circle" style="height: 72px;width:72px;" id="recordBtnRef"
+            :class="{'record-btn-ani': isRecording && !showTip}"
+            >
+            <!--  @mousedown="startRecording"
             @mouseup="stopRecording" 
             @touchstart="startRecording"
             @touchend="stopRecording" 
-            @touchmove.prevent="handleTouchMove" 
-            >
+            @touchmove.prevent="handleTouchMove"  -->
             <AudioOutlined :style="{ fontSize: '48px', color: isRecording ? null : 'white',userSelect:'none' }" />
             <!-- {{isRecording?'释放结束':'按住说话'}} -->
           </a-button>
         </div>
         <div v-else class="flex justify-center">
           <!-- 文件上传 -->
-          <a-upload beforeUpload="beforeUpload" :showUploadList="false">
-            <a-button>
-              <a-icon type="upload" /> Click to Upload
+          <!-- accept="audio/*,video/*" -->
+          <a-upload :beforeUpload="beforeUpload" :showUploadList="false" accept=".webm,.opus,.oga,.wma,.flac,.weba,.m4a,.wav,.ogg,.mp3,.ogm,.wmv,.mpg,.webm,.ogv,.mov,.asx">
+            <a-button style="height: 50px;width:120px">
+              <UploadOutlined />Upload
             </a-button>
           </a-upload>
         </div>
         <div class="float-right ">
+          <a-button type="text" @click="switchMode">切换模式</a-button>
           <a-button type="text" @click="settingOpen = true">高级设置</a-button>
         </div>
       </div>
@@ -282,11 +369,35 @@ const innerHeight = ref(window.innerHeight + 'px')
   
 <style >
 /* Add your custom styles here */
+@keyframes scale-toggle {
+  0% {
+    transform: scale(1);
+  }
+  25% {
+    transform: scale(0.9);
+  }
+  50% {
+    transform: scale(1);
+  }
+  75% {
+    transform: scale(1.1);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
 .my-container{
   /* @apply(h-screen) */
   height:100vh;
   padding-bottom: constant(safe-area-inset-bottom);
   padding-bottom: env(safe-area-inset-bottom);
+}
+.record-btn-ani {
+  animation-name: scale-toggle;
+  animation-duration: .7s;
+  animation-delay: 0;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
 }
 </style>
   
